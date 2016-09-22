be6faf9cc43b107a3a36befed5a029315c5b5981
Majer MichaÃÂ <michal.majer@hotmail.com>
2016-09-22 13:08:14 +0000

    Mobile adjustment + Tooltips added.

--- webapp/controller/Index.controller.js
+++ webapp/controller/Index.controller.js
@@ -7,16 +7,33 @@ sap.ui.define([
 
 	return Controller.extend("mm.controller.Index", {
 
+		oQuickViewNettoModel: new JSONModel(),
 		oSettlementModel: new JSONModel(),
 		oTaxFreeDataModel: new JSONModel(),
+		oQuickViewSocialContributeModel: new JSONModel(),
+		oQuickViewHealthContributeModel: new JSONModel(),
 
 		onInit: function() {
 			this.oB2B = new B2BCalculator(this);
+			
+			var mQuickViewNettoExplainData = {
+				pages: [{
+					pageId: "QuickViewNettoExplainId",
+					header: "Przychód netto",
+					groups: [{
+						heading: "Przychód netto",
+						elements: [{
+							label: "Opis",
+							value: "Kwota netto na wystawionych fakturach. Od tej kwoty zostanie obliczony podatek VAT. Suma kwoty netto i VAT stanowi kwotę brutto, którą otrzymasz jako wynagrodznie."
+						}]
+					}]
+				}]
+			};
 
 			var mSettlementData = {
 				pages: [{
 					pageId: "settlmentIdPage",
-					header: "Wybór formy opodatkowania:",
+					header: "Wybór formy opodatkowania",
 					groups: [{
 						heading: "Progresywna skala podatkowa",
 						elements: [{
@@ -42,20 +59,51 @@ sap.ui.define([
 			var mTaxFreeData = {
 				pages: [{
 					pageId: "taxFreeIdPage",
-					header: "Kwota wolna od podatku:",
+					header: "Kwota wolna od podatku",
 					groups: [{
 						heading: "Kwota wolna od podatku",
 						elements: [{
 							label: "Opis",
-							value: "Kwota jest wolna od opodatkowania jeśli nie przekracza 3091 zł w skali roku. Możliwość zmniejszenia podatku o 556,02 zł tylko gdy formą opodatkowania jest skala podatkowa."
+							value: "Kwota jest wolna od opodatkowania jeśli nie przekracza 3091 zł w skali roku. Możliwość zmniejszenia podatku o 556,02 zł rocznie tylko gdy formą opodatkowania jest skala podatkowa."
+						}]
+					}]
+				}]
+			};
+			
+			var mQuickViewSocialContributeExplainData = {
+				pages: [{
+					pageId: "QuickViewSocialContributeExplainId",
+					header: "Preferencyjna składka ZUS",
+					groups: [{
+						heading: "Obniżona składka społeczna",
+						elements: [{
+							label: "Opis",
+							value: "Osoby podejmujące działalność gospodarczą po raz pierwszy od 5 lat mają prawo do ograniczenia wysokości swoich składek ZUS w okresie pierwszych 24 miesięcy kalendarzowych od dnia rozpoczęcia wykonywania działalności gospodarczej."
+						}]
+					}]
+				}]
+			};
+			
+			var mQuickViewHealthContributeExplainData = {
+				pages: [{
+					pageId: "QuickViewSocialContributeExplainId",
+					header: "Ubezpieczenie chorobowe w ZUS",
+					groups: [{
+						heading: "Ubezpieczenie chorobowe",
+						elements: [{
+							label: "Opis",
+							value: "Każdy przedsiębiorca, który otwiera własną firmę, musi obowiązkowo zgłosić się z tego tytułu do ubezpieczeń w ZUS. Do ubezpieczenia chorobowego może natomiast przystąpić tylko na swój wyraźny wniosek. Jest ono bowiem dla niego dobrowolne. Aby jednak korzystać ze świadczeń chorobowych, musi m.in. terminowo opłacać składki ubezpieczeniowe."
 						}]
 					}]
 				}]
 			};
 
 			// set the data for the model
+			this.oQuickViewNettoModel.setData(mQuickViewNettoExplainData);
 			this.oSettlementModel.setData(mSettlementData);
 			this.oTaxFreeDataModel.setData(mTaxFreeData);
+			this.oQuickViewSocialContributeModel.setData(mQuickViewSocialContributeExplainData);
+			this.oQuickViewHealthContributeModel.setData(mQuickViewHealthContributeExplainData);
 		},
 
 		openQuickView: function(oEvent, oModel) {
@@ -72,6 +120,10 @@ sap.ui.define([
 				this._oQuickView.openBy(oButton);
 			});
 		},
+		
+		handleNettoQuickViewPress: function(oEvent) {
+			this.openQuickView(oEvent, this.oQuickViewNettoModel);
+		},
 
 		handleGenericQuickViewPress: function(oEvent) {
 			this.openQuickView(oEvent, this.oSettlementModel);
@@ -79,6 +131,14 @@ sap.ui.define([
 		
 		handleTaxFreeQuickViewPress: function(oEvent) {
 			this.openQuickView(oEvent, this.oTaxFreeDataModel);	
+		},
+		
+		handleSocialQuickViewPress: function(oEvent) {
+			this.openQuickView(oEvent, this.oQuickViewSocialContributeModel);
+		},
+		
+		handleHealthQuickViewPress: function(oEvent) {
+			this.openQuickView(oEvent, this.oQuickViewHealthContributeModel);
 		},
 
 		handlePopoverPress: function(oEvent) {
